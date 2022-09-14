705b35809f40fc11cd83ac0d3009b06066740fe2
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-13 21:16:41 -0300

    Criado o splash e a parte inicial do app - importado algumas bibs que serão usadas - criado componentes e estilizando-os

---
+++ mobile/src/screens/home/index.tsx
@@ -0,0 +1,39 @@
+import { View, Image, FlatList } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/gameCard';
+import { Heading } from '../../components/heading';
+
+import { GAMES } from '../../utils/games';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading 
+        title="Encontre seu Duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard 
+            data={item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
+
+    </View>
+  );
+}
