f3b877a60542755d40b74ccc937b20f549c0eccb
Matheus Viscki <62353431+mathvsk@users.noreply.github.com>
2022-09-17 14:11:49 -0300

    Integrando o App mobile com o back-end - criado as rotas - criado outra 'pagina' dentro do app - criado eventos - criado componentes e estilizações - adicionado bibs para navegação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 
-import { Home } from './src/screens/home';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/loading';
 import { Background } from './src/components/background';
 
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Home /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
